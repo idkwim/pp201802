9b16ba2b1cd293cc0ff808f30dfc84523291f1e2
Dongjoo-Kim <kimdj316@naver.com>
2018-12-21 01:48:00 +0900

    Update compile.bat

--- Project/compile.bat
+++ Project/compile.bat
@@ -1,4 +1,5 @@
 rd /s /q classes
 mkdir classes
-call scalac -classpath lib/proj-lib.jar:classes/ -d classes/ src/Main.scala
-call scalac -classpath lib/proj-lib.jar:classes/ -d classes/ src/Test.scala
+call scalac -classpath "lib/proj-lib.jar;classes/" -d classes/ src/Main.scala
+call scalac -classpath "lib/proj-lib.jar;classes/" -d classes/ src/MyParser.scala
+call scalac -classpath "lib/proj-lib.jar;classes/" -d classes/ src/Test.scala
